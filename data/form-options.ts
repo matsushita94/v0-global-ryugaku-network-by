5b099000154115d7579525362822c19ff5b94f83
matsushita94 <matsushita1994@gmail.com>
2026-04-01 15:03:30 +0900

    Create form-options.ts

---
+++ data/form-options.ts
@@ -0,0 +1,38 @@
+export const programTypes = [
+  "Language School",
+  "Vocational School",
+  "University",
+  "Graduate School",
+  "Not Sure Yet",
+]
+
+export const japaneseLevels = [
+  "None (No Japanese)",
+  "Beginner",
+  "Elementary",
+  "Intermediate",
+  "Advanced",
+  "N5",
+  "N4",
+  "N3",
+  "N2",
+  "N1",
+  "Native / Fluent",
+]
+
+export const urgencyOptions = [
+  "As soon as possible",
+  "Within 3 months",
+  "Within 6 months",
+  "Just exploring",
+]
+
+export const preferredContactMethods = ["Email", "Phone"]
+
+export const budgetRanges = [
+  "Under ¥500,000",
+  "¥500,000 – ¥1,000,000",
+  "¥1,000,000 – ¥2,000,000",
+  "¥2,000,000+",
+  "Not Sure Yet",
+]
